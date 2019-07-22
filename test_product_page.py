0405536d33da28a6e7a242ebb0334ed3d6ae0fb3
Driver3d <45138033+Driver3d@users.noreply.github.com>
2019-07-22 11:36:44 +0300

    Test THE END 2git add *!

--- test_product_page.py
+++ test_product_page.py
@@ -6,13 +6,12 @@ from pages.locators import LoginCartPageLocators
 from pages.cart_page import CartPage
 from pages.login_page import LoginPage
 
-product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
+# product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+# urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
 
-
-@pytest.mark.parametrize('link', urls)
-def test_guest_can_add_product_to_cart(browser, link):
-	# link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.need_review
+def test_guest_can_add_product_to_cart(browser):
+	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 	page = ProductPage(browser, link)
 	page.open()
 	page.test_add_cart()
@@ -29,13 +28,15 @@ def test_guest_should_see_login_link_on_product_page(browser):
 	page = ProductPage(browser, link)
 	page.open()
 	page.should_be_login_link()
-
+	
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 	page = ProductPage(browser, link)
 	page.open()
 	page.go_to_login_page()
-		
+
+@pytest.mark.need_review	
 def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 	page = CartPage(browser, link)
@@ -55,6 +56,7 @@ class TestUserAddToCartFromProductPage(object):
 		page.register_button()
 		page.should_be_authorized_user()
 
+	@pytest.mark.need_review
 	def test_user_cant_see_success_message(self, browser):
 		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 		page = ProductPage(browser, link)
